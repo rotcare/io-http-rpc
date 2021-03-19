a839a03d5c08be43c7e75da815174e0224830d7a
Tao Wen <taowen@chengfayun.com>
2021-03-19 13:47:42 +0800

    remove dependency on node.js path package

--- src/generateHttpRpcServers.ts
+++ src/generateHttpRpcServers.ts
@@ -1,11 +1,11 @@
 import { HttpRpcServer } from "./HttpRpcServer";
 import { Model } from '@rotcare/codegen';
-import * as path from 'path';
 
 export function generateHttpRpcServers(models: Model[]): Record<string, HttpRpcServer> {
     const lines = [`
+    const { HttpRpcServer } = require('@rotcare/io-http-rpc');
     const httpRpcServers = {
-        migrate: new Impl.HttpRpcServer({ func: require('@motherboard/migrate').migrate })
+        migrate: new HttpRpcServer({ func: require('@motherboard/migrate').migrate })
     };`];
     models.sort((a, b) => a.qualifiedName.localeCompare(b.qualifiedName));
     for (const model of models) {
@@ -17,10 +17,10 @@ export function generateHttpRpcServers(models: Model[]): Record<string, HttpRpcS
             ...model.staticMethods.map((m) => m.name),
         ];
         for (const service of services) {
-            const className = path.basename(model.qualifiedName);
+            const className = model.qualifiedName.substr(model.qualifiedName.lastIndexOf('/') + 1);
             lines.push(
                 [
-                    `httpRpcServers.${service} = Impl.HttpRpcServer.create(`,
+                    `httpRpcServers.${service} = HttpRpcServer.create(`,
                     `() => import('@motherboard/${model.qualifiedName}'), `,
                     `'${className}', '${service}');`,
                 ].join(''),
